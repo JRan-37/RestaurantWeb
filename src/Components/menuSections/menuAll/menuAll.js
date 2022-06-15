d787022382c273352bfe27434c72b10118a1f1f1
JRan-37 <79283020+JRan-37@users.noreply.github.com>
2022-06-15 09:14:53 -0400

    menu duplicate category fix

--- src/Components/menuSections/menuAll/menuAll.js
+++ src/Components/menuSections/menuAll/menuAll.js
@@ -6,9 +6,11 @@ import Modal from '../../navigation/modal/modal';
 import Backdrop from '../../navigation/backdrop/backdrop';
 import { Container, Row, Col } from 'reactstrap';
 import {Button} from 'reactstrap';
+import axios from 'axios';
 
 class menuAll extends Component {
     state = {
+        categories: [],
         options: [],
         load:"Select From Above",
         showCustomize:false,
@@ -25,9 +27,19 @@ class menuAll extends Component {
         this.setState({load:name,sendData:this.props.menudata});
       
     }
+
+    componentDidMount(){
+        axios.get("http://localhost:8080/api/menu/categories").then((response)=>{
+            this.setState({categories:response.data});
+            console.log(this.categories);
+        }).catch(err=>console.log("Didn't load")).then(console.log("Unable to get data for menu categories"));
+      }
+
+
     render() {
         //let menuitems = this.props.menudata.map(item => <Single name={item.category} key={item.category} click={this.loadStateHandler} />);
         let menuitems = Object.keys(this.props.menudata).map(id => <Single name={this.props.menudata[id].category} key={this.props.menudata[id].id} click={this.loadStateHandler} />);
+        let menucategories = Object.keys(this.state.categories).map(id => <Single name={this.state.categories[id].category} key={this.state.categories[id].category} click={this.loadStateHandler} />);
         //var arr = removeDuplicatesBy(x => x.price, menuitems);
         //menuitems.filter((v,i,a)=>a.findIndex(v2=>(v2.category===v.category))===i.cat);
         //let opn = Object.keys(this.props.data).map(data => <Single  name={data} click={this.loadStateHandler}  key={data} />);
@@ -90,7 +102,7 @@ class menuAll extends Component {
         let outPut=(
             <div className="menuAll">
             {/*opn*/}
-            {menuitems}
+            {menucategories}
             <Inoptions adding={this.props.adding} selected={this.state.load} data={this.state.sendData} customization={this.openCustomizeHandler} check={menuitems} />
             {custResult}
         </div>
