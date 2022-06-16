30c4922296b6e7ecf2f384bc3b8230d83911a41c
Tim Jonker <tmjonker1@outlook.com>
2022-06-16 09:25:15 -0400

    Fixed bug that allowed items to stay in cart after removal.

--- src/Components/Cart/Items/Items.js
+++ src/Components/Cart/Items/Items.js
@@ -2,11 +2,14 @@ import React from 'react';
 import Item from './Item/Item';
 import {Container} from 'reactstrap';
 const Items=(props)=>{
+
+    let filteredCart = props.data.filter(element => element.counter > 0);
+
     return(
         <div className="Items">
             <Container>
-                
-            {props.data.map(element=>{
+
+            {filteredCart.map(element=>{
                 return <Item key={element.head} decrement={props.remove} increment={props.adding} head={element.head} cnt={element.counter} price={element.price} url={element.url} content={element.content} />
         })}
             </Container>
